d8f48819b027aee05229be3cc07a55340f612da6
Jonas Have <B050080@PROD.SITAD.DK>
2019-12-20 09:52:16 +0100

    Added errorhandling to run function

--- autochange/run.py
+++ autochange/run.py
@@ -6,7 +6,7 @@ from . import filehandling
 def run(path, patterns, process_func):
     '''
     Function that runs two seperate threads. One thread is watching
-    a directory and adds file created events to a queue. The other 
+    a directory and adds file created events to a queue. The other
     thread checks the queue for new events. If events are in the
     queue the files are processed according to the process_func
     function.
@@ -21,14 +21,22 @@ def run(path, patterns, process_func):
     event_queue = queue.Queue()
     abort_event = threading.Event()
 
-    # Set up a worker thread to process database load
-    worker = threading.Thread(target=filehandling.process_queue,
-                              args=(process_func, event_queue, 
-                                    abort_event, path,))
-    worker.setDaemon(True)
-    worker.start()
+    try:
+        # start a daemon thread to process files
+        worker = threading.Thread(target=filehandling.process_queue,
+                                  args=(process_func, event_queue,
+                                        abort_event, path,))
+        worker.setDaemon(True)
+        worker.start()
 
-    # Start another thread that watches a directory for files
-    # matching a pattern
-    w = filehandling.Watcher()
-    w.run(path, event_queue, abort_event, match_patterns=patterns)
+        # start another thread that watches a directory for files
+        # matching a pattern
+        w = filehandling.Watcher()
+        w.run(path, event_queue, abort_event, match_patterns=patterns)
+
+    except FileNotFoundError:
+        print("The path supplied does not point to a valid directory")
+
+    except TypeError:
+        print("The process function supplied is not valid. "
+              "Only functions taking a single input can be used.")
